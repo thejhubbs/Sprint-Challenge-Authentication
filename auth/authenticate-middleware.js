99cc9ea2c392c1243a963f26857e8c56bb91045e
thejhubbs <thejhubbs@gmail.com>
2019-09-14 13:29:14 -0400

    Created auth/users model

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,16 @@
-/* 
+/*
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
 
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
-};
+function restricted(req, res, next) {
+  const user = req.session.user
+  console.log("checking", req.session.user, user, req.session)
+  if(user) {
+    next();
+  } else {
+    res.status(400).json({message: "Please log in."})
+  }
+}
+
+module.exports = restricted
